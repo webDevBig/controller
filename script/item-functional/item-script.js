65064b3b6b1f67a64caad7779927770a20fb8be9
DEVBIG <olenadev7@gmail.com>
2023-03-29 15:30:34 +0300

    add icons

--- script/item-functional/item-script.js
+++ script/item-functional/item-script.js
@@ -2,24 +2,23 @@
  let modalBackground = document.getElementById('modal-source');
  let source_value = document.querySelector('#modal-source span');
  let source_img = document.querySelector('#modal-source i');
-//  var source_name = 
+ //  var source_name = 
  [].forEach.call(source_item, function (el) {
      el.onclick = function (e) {
          for (var i = 0; i < source_item.length; i++) {
-            source_item[i].classList.remove('active');
+             source_item[i].classList.remove('active');
          }
          var v = el.querySelector('.source-name').innerHTML
          el.classList.toggle('active');
          source_value.innerHTML = v;
          modalBackground.style.display = 'block';
-        //  source_img.setAttribute("src", "url(../../img/modal-icon/" + v + "-active.svg");
-        //  source_img.style.mask = 'url(../../img/source-icon/' + v + '.svg) no-repeat center;'
+         source_img.setAttribute("class", v);
 
          function page() {
-            window.open(v + '.html', "_self")
-        }
-        // setTimeout(function () {
-        //     page()
-        // }, 2500);
+             window.open(v + '.html', "_self")
+         }
+         // setTimeout(function () {
+         //     page()
+         // }, 2500);
      }
  });
